e4197309428df848f024438c13a2ff9004afa563
araishi17 <araishikudo@gmail.com>
2025-06-11 13:33:33 +0700

    Create home_screen.dart

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:provider/provider.dart';
+import '../services/auth_service.dart';
+import '../services/firestore_service.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final authService = Provider.of<AuthService>(context...
+
+Ada kesalahan teknis, silakan refresh untuk menyambungkan kembali atau coba lagi.
